80dcc520dab37c74faad4fb29e391bb0ba994834
amitpatel <patelamit812@gmai.com>
2025-03-09 16:57:52 +0530

    debugging spring security internal class

---
+++ src/main/java/com/security/learn/security/SecurityConfig.java
@@ -0,0 +1,38 @@
+package com.security.learn.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@EnableWebSecurity(debug = true)
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        //using without database
+       UserDetails userDetails1 = User.withDefaultPasswordEncoder()
+                .username("amit")
+                .password("amit")
+                .roles("ADMIN","GUEST")
+                .build();
+
+        UserDetails userDetails2 = User.withDefaultPasswordEncoder()
+                .username("nikki")
+                .password("nikki")
+                .roles("ADMIN")
+                .build();
+
+        InMemoryUserDetailsManager inMemoryUserDetailsManager = new InMemoryUserDetailsManager(userDetails1,userDetails2);
+
+        return inMemoryUserDetailsManager;
+    }
+
+
+
+
+}
